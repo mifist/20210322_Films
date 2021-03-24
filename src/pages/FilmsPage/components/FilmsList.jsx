7d9f6a1c25b921505d38d1ca633f1e041a1c4aec
romannikiforov <escaro@ukr.net>
2021-03-24 19:22:38 +0200

    ready Message snippet 5

--- src/pages/FilmsPage/components/FilmsList.jsx
+++ src/pages/FilmsPage/components/FilmsList.jsx
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 import FilmCard from "pages/FilmsPage/components/FilmCard";
+import Message from "components/Message";
 
 const FilmsList = ({ films }) => {
   return (
     <div className="ui four cards">
-      {films.map((film) => (
-        <FilmCard key={film._id} film={film} />
-      ))}
+      {films.length === 0 ? (
+        <Message type="star" color="orange">
+          No films in our base yet
+        </Message>
+      ) : (
+        films.map((film) => <FilmCard key={film._id} film={film} />)
+      )}
     </div>
   );
 };
