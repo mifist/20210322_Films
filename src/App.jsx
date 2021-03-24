55aaeb33d43d24800e9557c4bae63ba8fba86421
romannikiforov <escaro@ukr.net>
2021-03-24 19:26:42 +0200

    sort with lodash

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import _sortBy from "lodash/sortBy";
 import FilmsList from "pages/FilmsPage/components/FilmsList";
 import { films } from "data";
+import _ from "lodash";
 
 class App extends Component {
   state = {
     films: [],
   };
 
+  sortFilms = (films) => _sortBy(films, ["title"]);
+
   componentDidMount() {
-    this.setState({ films });
+    this.setState({ films: this.sortFilms(films) });
   }
 
   onReset = () => this.setState({ films });
