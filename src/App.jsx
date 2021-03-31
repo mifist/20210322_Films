cedbc76b6b75d4d52005fcbf10ef52995a16abe4
roman <escaro@ukr.net>
2021-03-31 21:05:44 +0300

    ready snippet 3 lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import TopNavigation from "components/TopNavigation";
 import HomePage from "pages/HomePage";
-import FilmsPage from "pages/FilmsPage";
-import SignupPage from "pages/SignupPage";
-import LoginPage from "pages/LoginPage";
+import { FullSpinner } from "styles/app";
+
+const FilmsPage = lazy(() => import("pages/FilmsPage"));
+const SignupPage = lazy(() => import("pages/SignupPage"));
+const LoginPage = lazy(() => import("pages/LoginPage"));
 
 const initUser = {
   token: null,
@@ -21,22 +23,24 @@ class App extends Component {
   render() {
     const { user } = this.state;
     return (
-      <div className="ui container mt-3">
-        <TopNavigation logout={this.logout} isAuth={!!user.token} />
+      <Suspense fallback={FullSpinner}>
+        <div className="ui container mt-3">
+          <TopNavigation logout={this.logout} isAuth={!!user.token} />
 
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/films">
-          <FilmsPage />
-        </Route>
-        <Route path="/signup">
-          <SignupPage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-      </div>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/films">
+            <FilmsPage />
+          </Route>
+          <Route path="/signup">
+            <SignupPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+        </div>
+      </Suspense>
     );
   }
 }
