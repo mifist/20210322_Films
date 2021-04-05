8ac16d495611b5cd503588baf317816976c9c3d2
roman <escaro@ukr.net>
2021-04-05 12:50:47 +0300

    add validate email in LoginForm

--- src/pages/LoginPage/components/LoginForm.jsx
+++ src/pages/LoginPage/components/LoginForm.jsx
@@ -1,62 +1,77 @@
-import {Component} from "react"
-import {Link} from "react-router-dom"
-import FormMessage from "components/FormMessage"
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+import isEmail from "validator/es/lib/isEmail";
+import FormMessage from "components/FormMessage";
 
 const initialData = {
   email: "",
   password: "",
-}
+};
 
 class LoginForm extends Component {
   state = {
     data: initialData,
     errors: {},
     loading: false,
-  }
-  handleChange = e =>
+  };
+  handleChange = (e) =>
     this.setState({
-      data: {...this.state.data, [e.target.name]: e.target.value},
-       errors: {...this.state.errors, [e.target.name]: ""},
-    })
+      data: { ...this.state.data, [e.target.name]: e.target.value },
+      errors: { ...this.state.errors, [e.target.name]: "" },
+    });
 
   validate(data) {
-    const errors = {}
-    if (!data.email) errors.email = "Email cannot be blank"
-    if (!data.password) errors.password = "Password cannot be blank"
+    const errors = {};
+    if (!isEmail(data.email)) errors.email = "wrong format email";
+    if (!data.password) errors.password = "Password cannot be blank";
 
-    return errors
+    return errors;
   }
 
-  handleSubmit = e => {
-    e.preventDefault()
-    const errors = this.validate(this.state.data)
-    this.setState({errors})
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
     if (Object.keys(errors).length === 0) {
-      this.setState({loading: true})
+      this.setState({ loading: true });
       this.props
         .submit(this.state.data)
-        .catch(error =>
-          this.setState({errors: error.response.data.errors, loading: false}),
-        )
+        .catch((error) =>
+          this.setState({ errors: error.response.data.errors, loading: false })
+        );
     }
-  }
-  
+  };
 
   render() {
-    const {data, errors, loading} = this.state
-    const cls = loading ? "ui form loading" : "ui form"
+    const { data, errors, loading } = this.state;
+    const cls = loading ? "ui form loading" : "ui form";
     return (
       <form className={cls} onSubmit={this.handleSubmit}>
         <div className={errors.email ? "error field" : "field"}>
           <label>Email</label>
-          <input value={data.email} onChange={this.handleChange} type="text"  name="email" id="email"  placeholder="Email" />
+          <input
+            value={data.email}
+            onChange={this.handleChange}
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
           {errors.email && <FormMessage>{errors.email}</FormMessage>}
         </div>
 
         <div className={errors.password ? "error field" : "field"}>
           <label>Password</label>
-          <input value={data.password} onChange={this.handleChange} type="text" name="password" id="password" placeholder="password" />
-          {errors.password  &&   <FormMessage>{errors.password}</FormMessage>} 
+          <input
+            value={data.password}
+            onChange={this.handleChange}
+            type="text"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+          {errors.password && <FormMessage>{errors.password}</FormMessage>}
         </div>
         <div className="ui fluid buttons">
           <button className="ui button primary">Login</button>
@@ -68,8 +83,8 @@ class LoginForm extends Component {
           </Link>
         </div>
       </form>
-    )
+    );
   }
 }
 
-export default LoginForm
+export default LoginForm;
