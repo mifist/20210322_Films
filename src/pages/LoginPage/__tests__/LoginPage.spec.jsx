7cff08de0a224ab7ed1c0382ccc2ab96bdcd73b0
roman <escaro@ukr.net>
2021-04-05 21:22:52 +0300

    correct render LoginPage

--- src/pages/LoginPage/__tests__/LoginPage.spec.jsx
+++ src/pages/LoginPage/__tests__/LoginPage.spec.jsx
@@ -1,1 +1,25 @@
-test("should correct render", () => {});
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter as Router } from "react-router-dom";
+import LoginPage from "pages/LoginPage";
+import { UserContextProvider } from "contexts/UserContext";
+import mockApi from "api";
+
+const fakeData = { email: "u1@com.ua", password: "secret" };
+
+test("should correct render", () => {
+  render(
+    <Router>
+      <LoginPage />
+    </Router>,
+    { wrapper: UserContextProvider }
+  );
+  const emailEl = screen.getByLabelText(/email/i);
+  const passwordEl = screen.getByLabelText(/password/i);
+  const btnEl = screen.getByText(/login/i);
+
+  userEvent.type(emailEl, fakeData.email);
+  userEvent.type(passwordEl, fakeData.password);
+
+  userEvent.click(btnEl);
+});
