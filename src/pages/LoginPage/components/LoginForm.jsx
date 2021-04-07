847f57addea9a239e20aea0f973aea9e644579db
roman <escaro@ukr.net>
2021-04-07 20:46:22 +0300

    ready FormUtils

--- src/pages/LoginPage/components/LoginForm.jsx
+++ src/pages/LoginPage/components/LoginForm.jsx
@@ -1,90 +1,79 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
 import isEmail from "validator/lib/isEmail";
 import FormMessage from "components/FormMessage";
+import setFormObj from "components/FormUtils";
 
 const initialData = {
   email: "",
   password: "",
 };
 
-class LoginForm extends Component {
-  state = {
-    data: initialData,
-    errors: {},
-    loading: false,
-  };
-  handleChange = (e) =>
-    this.setState({
-      data: { ...this.state.data, [e.target.name]: e.target.value },
-      errors: { ...this.state.errors, [e.target.name]: "" },
-    });
+const LoginForm = ({ submit }) => {
+  const [data, setData] = useState(initialData);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  validate(data) {
+  const validate = (data) => {
     const errors = {};
     if (!isEmail(data.email)) errors.email = "wrong format email";
     if (!data.password) errors.password = "Password cannot be blank";
 
     return errors;
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validate(this.state.data);
-    this.setState({ errors });
-    if (Object.keys(errors).length === 0) {
-      this.setState({ loading: true });
-      this.props
-        .submit(this.state.data)
-        .catch((error) =>
-          this.setState({ errors: error.response.data.errors, loading: false })
-        );
-    }
   };
 
-  render() {
-    const { data, errors, loading } = this.state;
-    const cls = loading ? "ui form loading" : "ui form";
-    return (
-      <form data-testid="login-form"  className={cls} onSubmit={this.handleSubmit}>
-        <div className={errors.email ? "error field" : "field"}>
-          <label htmlFor="email">Email</label>
-          <input
-            value={data.email}
-            onChange={this.handleChange}
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email"
-          />
-          {errors.email && <FormMessage>{errors.email}</FormMessage>}
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validate(data);
+    setErrors(errors);
 
-        <div className={errors.password ? "error field" : "field"}>
-          <label htmlFor="password">Password</label>
-          <input
-            value={data.password}
-            onChange={this.handleChange}
-            type="text"
-            name="password"
-            id="password"
-            placeholder="password"
-          />
-          {errors.password && <FormMessage>{errors.password}</FormMessage>}
-        </div>
-        <div className="ui fluid buttons">
-          <button className="ui button primary">Login</button>
+    if (Object.keys(errors).length === 0) {
+      setLoading(true);
+      submit(data).catch((error) => {
+        setErrors(error.response.data.errors);
+        setLoading(false);
+      });
+    }
+  };
+  const cls = loading ? "ui form loading" : "ui form";
+  return (
+    <form data-testid="login-form" className={cls} onSubmit={handleSubmit}>
+      <div className={errors.email ? "error field" : "field"}>
+        <label htmlFor="email">Email</label>
+        <input
+          value={data.email}
+          onChange={setFormObj(data, setData)}
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email"
+        />
+        {errors.email && <FormMessage>{errors.email}</FormMessage>}
+      </div>
 
-          <div className="or" />
+      <div className={errors.password ? "error field" : "field"}>
+        <label htmlFor="password">Password</label>
+        <input
+          value={data.password}
+          onChange={setFormObj(data, setData)}
+          type="text"
+          name="password"
+          id="password"
+          placeholder="password"
+        />
+        {errors.password && <FormMessage>{errors.password}</FormMessage>}
+      </div>
+      <div className="ui fluid buttons">
+        <button className="ui button primary">Login</button>
 
-          <Link to="/" className="ui button">
-            Cancel
-          </Link>
-        </div>
-      </form>
-    );
-  }
-}
+        <div className="or" />
+
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </div>
+    </form>
+  );
+};
 
 export default LoginForm;
